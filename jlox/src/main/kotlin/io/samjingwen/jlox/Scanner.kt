3d34284b6f70fddc7293ea76e6247f5a155e61ef
samjingwen <samjingwen@u.nus.edu>
2022-08-23 20:08:32 +0800

    added jlox

---
+++ jlox/src/main/kotlin/io/samjingwen/jlox/Scanner.kt
@@ -0,0 +1,176 @@
+package io.samjingwen.jlox
+
+import io.samjingwen.jlox.TokenType.*
+
+class Scanner(source: String) {
+    private val source: String
+    private val tokens: MutableList<Token>
+
+    private var start = 0
+    private var current = 0
+    private var line = 1
+
+    init {
+        this.source = source
+        this.tokens = mutableListOf()
+    }
+
+    fun scanTokens(): List<Token> {
+        while (!isAtEnd()) {
+            start = current
+            scanToken()
+        }
+
+        tokens.add(Token(EOF, "", null, line))
+        return tokens
+    }
+
+    private fun isAtEnd(): Boolean {
+        return current >= source.length
+    }
+
+    private fun scanToken() {
+        when (val c = advance()) {
+            '(' -> addToken(LEFT_PAREN)
+            ')' -> addToken(RIGHT_PAREN)
+            '{' -> addToken(LEFT_BRACE)
+            '}' -> addToken(RIGHT_BRACE)
+            ',' -> addToken(COMMA)
+            '.' -> addToken(DOT)
+            '-' -> addToken(MINUS)
+            '+' -> addToken(PLUS)
+            ';' -> addToken(SEMICOLON)
+            '*' -> addToken(STAR)
+            '!' -> addToken(if (match('=')) BANG_EQUAL else BANG)
+            '=' -> addToken(if (match('=')) EQUAL_EQUAL else EQUAL)
+            '<' -> addToken(if (match('=')) LESS_EQUAL else LESS)
+            '>' -> addToken(if (match('=')) GREATER_EQUAL else GREATER)
+            '/' -> {
+                if (match('/')) {
+                    // A comment goes until the end of the line.
+                    while (peek() != '\n' && !isAtEnd()) advance()
+                } else {
+                    addToken(SLASH)
+                }
+            }
+
+            ' ', '\r', '\t' -> Unit
+            '\n' -> line++
+            '"' -> string()
+            else -> {
+                if (isDigit(c)) {
+                    number()
+                } else if (isAlpha(c)) {
+                    identifier()
+                } else {
+                    Lox.error(line, "Unexpected character.")
+                }
+            }
+        }
+    }
+
+    private fun advance(): Char {
+        return source[current++]
+    }
+
+    private fun addToken(type: TokenType) {
+        addToken(type, null)
+    }
+
+    private fun addToken(type: TokenType, literal: Any?) {
+        val text = source.substring(start, current)
+        tokens.add(Token(type, text, literal, line))
+    }
+
+    private fun match(expected: Char): Boolean {
+        if (isAtEnd()) return false
+        if (source[current] != expected) return false
+        current++
+        return true
+    }
+
+    private fun peek(): Char {
+        if (isAtEnd()) return '\u0000'
+        return source[current]
+    }
+
+    private fun peekNext(): Char {
+        if (current + 1 >= source.length) {
+            return '\u0000'
+        }
+        return source[current + 1]
+    }
+
+    private fun string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++
+            advance()
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.")
+            return
+        }
+
+        // The closing ".
+        advance()
+
+        // Trim the surrounding quotes.
+        val value = source.substring(start + 1, current - 1)
+        addToken(STRING, value)
+    }
+
+    private fun isDigit(c: Char): Boolean {
+        return c in '0'..'9'
+    }
+
+    private fun number() {
+        while (isDigit(peek())) advance()
+
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance()
+
+            while (isDigit(peek())) advance()
+        }
+
+        addToken(NUMBER, source.substring(start, current).toDouble())
+    }
+
+    private fun identifier() {
+        while (isAlphaNumeric(peek())) advance()
+
+        val text = source.substring(start, current)
+        val type = keywords[text] ?: IDENTIFIER
+
+        addToken(type)
+    }
+
+    private fun isAlpha(c: Char): Boolean {
+        return (c in 'a'..'z') || (c in 'A'..'Z') || c == '_'
+    }
+
+    private fun isAlphaNumeric(c: Char): Boolean {
+        return isAlpha(c) || isDigit(c)
+    }
+
+    companion object {
+        val keywords = mapOf(
+            "and" to AND,
+            "class" to CLASS,
+            "else" to ELSE,
+            "false" to FALSE,
+            "for" to FOR,
+            "fun" to FUN,
+            "if" to IF,
+            "nil" to NIL,
+            "or" to OR,
+            "print" to PRINT,
+            "return" to RETURN,
+            "super" to SUPER,
+            "this" to THIS,
+            "true" to TRUE,
+            "var" to VAR,
+            "while" to WHILE
+        )
+    }
+}
---
+++ jlox/src/main/kotlin/io/samjingwen/jlox/Token.kt
@@ -0,0 +1,19 @@
+package io.samjingwen.jlox
+
+class Token(type: TokenType, lexeme: String, literal: Any?, line: Int) {
+    private val type: TokenType
+    private val lexeme: String
+    private val literal: Any?
+    private val line: Int
+
+    init {
+        this.type = type
+        this.lexeme = lexeme
+        this.literal = literal
+        this.line = line
+    }
+
+    override fun toString(): String {
+        return "$type $lexeme $literal"
+    }
+}
